72d3716a39cb607ff8b9a37d6e122057d3313b66
nucfox <jiang9217@foxmail.com>
2022-02-17 15:46:18 +0800

    改权限

--- flink-scala_2.11-java11-debian/docker-entrypoint.sh
+++ flink-scala_2.11-java11-debian/docker-entrypoint.sh
@@ -31,10 +31,10 @@ drop_privs_cmd() {
         return
     elif [ -x /sbin/su-exec ]; then
         # Alpine
-        echo su-exec flink
+        echo su-exec flinkm
     else
         # Others
-        echo gosu root
+        echo gosu flinkm
     fi
 }
 
