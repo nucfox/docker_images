5520925e3652be118cd235ea07adf113c9d954ae
nucfox <jiang9217@foxmail.com>
2022-03-29 17:29:58 +0800

    up

--- flink/docker-entrypoint.sh
+++ flink/docker-entrypoint.sh
@@ -102,7 +102,7 @@ prepare_configuration
 
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
-envsubst < ${HOME}/passwd.template > /tmp/passwd
+envsubst < /opt/flink/passwd.template > /tmp/passwd
 export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
