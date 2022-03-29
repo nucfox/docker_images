99ec0cf94e1777c486dd8def3a8239e27848ae3f
nucfox <jiang9217@foxmail.com>
2022-03-29 17:58:38 +0800

    add jemalloc

--- flink/docker-entrypoint.sh
+++ flink/docker-entrypoint.sh
@@ -90,7 +90,7 @@ prepare_configuration() {
 
 maybe_enable_jemalloc() {
     if [ "${DISABLE_JEMALLOC:-false}" == "false" ]; then
-        export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libjemalloc.so
+        export LD_PRELOAD=$LD_PRELOAD:/usr/lib64/libjemalloc.so.1
     fi
 }
 
